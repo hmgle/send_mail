2cd99af86812fba17ac76e21f4dce7fdde80e47d
mingang.he <dustgle@gmail.com>
2013-03-21 20:01:51 +0800

    added README.md and send_mail.sh

---
+++ send_mail.sh
@@ -0,0 +1,51 @@
+#!/bin/sh
+
+set -x
+
+sender=$1
+reciver=$2
+subject=$3
+email_content_txt=$4
+mailserver=$5
+smtp_auth_user=$6
+smtp_auth_pwd=$7
+
+if [ "$#" != 7 ]; then
+    echo
+    echo "Usage: $0 sender@sever.com rec@domain.com subject mail_content_file mailserver user pwd"
+    echo
+    exit 3
+fi
+
+mail_content()
+{
+    # echo "in mail_content"
+cat <<EOF
+To:$reciver
+From:$sender
+Subject: $subject
+Date: `date` +0800
+Mime-Version: 1.0
+Content-Type: text/plain; charset="gb2312"; format=flowed
+
+ip camera test body
+EOF
+    test -r $email_content_txt && cat $email_content_txt
+}
+
+send_mail()
+{
+    echo "in send_mail"
+    (
+    echo $smtp_auth_user
+    sleep 7
+    for comm in "helo localhost" "auth login" $smtp_auth_user "$smtp_auth_pwd" "MAIL FROM:<$sender>" "RCPT TO:<$reciver>" "data"
+    do
+        echo "$comm";sleep 5
+    done
+    mail_content
+    sleep 4; echo "."; echo "q"
+    ) | telnet $mailserver 25
+}
+
+send_mail 
